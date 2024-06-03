383206faf7f7062312e69b6cd2aacf01e7c633ed
Ryan Coe <rcoe@sandia.gov>
2024-06-03 13:09:49 -0600

    update from template to fit this project structure

---
+++ test_wec_as_multiport.py
@@ -0,0 +1,67 @@
+
+import pytest
+
+import numpy as np
+
+import wec_as_multiport as wam
+import wecopttool as wot
+
+bem_data_fname = 'wec_as_multiport.nc'
+
+
+@pytest.fixture(scope="module")
+def wec():
+    bem_data = wot.read_netcdf(bem_data_fname)
+    wec = wam.WEC(omega=bem_data['omega'].values,
+                 N=12.4666,
+                 Kt=6.1745,
+                 Rw=0.5,
+                 Lw=0,
+                 Jd=2,
+                 Bd=1,
+                 Kd=0,
+                 Zi=np.squeeze(wot.hydrodynamic_impedance(bem_data)).values,
+                 Hexc=np.squeeze(bem_data['excitation_force'].values))
+    return wec
+
+
+def test_low_freq_wave_hydrostatics(wec):
+    """Excitation force at low frequency should approach hydrostatic 
+    stiffness"""
+
+    wave = wot.waves.regular_wave(f1=wec.f1, nfreq=wec.nfreq,
+                                  freq=wec.f1, amplitude=1)
+
+    Fexc = np.abs(wec.Fexc(wave.squeeze().values))[0]
+
+    Kh = 0.88**2 * np.pi * 1e3 * 9.81
+    Fhs = 1*Kh
+
+    assert Fexc == pytest.approx(Fhs, rel=1e-1)
+
+
+def test_Zl_opt(wec):
+    """Optimal load based on bi-conjugate impedance matching condition
+    should agree with optimal load based on Thevenin equivalent circuit
+    """
+
+    Zl_opt_biconj = wec.Zl_opt
+    Zl_opt_Thevenin = np.conj(wec.Z_Thevenin)
+
+    assert Zl_opt_biconj == pytest.approx(Zl_opt_Thevenin)
+
+
+@pytest.mark.parametrize("wave_freq", [0.2, 0.3, 0.4, 0.5, 0.6, 0.65])
+def test_max_active_power(wec, wave_freq):
+    """Active power when using a load defined by the bi-conjugate matching 
+    condition should match the maximum power per the Thevenin equivalent 
+    system"""
+
+    wave = wot.waves.regular_wave(f1=wec.f1, nfreq=wec.nfreq,
+                                  freq=wave_freq, amplitude=1)
+
+    Fexc = wec.Fexc(wave.squeeze().values)
+    max_pow = wec.max_active_power(Fexc)
+    pow = wec.active_power(Fexc)
+
+    assert pow == pytest.approx(max_pow)
---
+++ wec_as_multiport.py
@@ -0,0 +1,163 @@
+
+import numpy as np
+import copy
+
+# import capytaine as cpy
+# import wecopttool as wot
+
+
+class WEC:
+
+    def __init__(self, omega, N, Kt, Rw, Lw, Jd, Bd, Kd, Zi, Hexc) -> None:
+
+        self.omega = omega
+        self.N = N
+        self.Kt = Kt
+        self.Rw = Rw
+        self.Lw = Lw
+        self.Jd = Jd
+        self.Bd = Bd
+        self.Kd = Kd
+
+        self.Zi = Zi
+        self.Hexc = Hexc
+
+    # def __repr__(self) -> str:
+    #     type_ = type(self)
+    #     module = type_.__module__
+    #     qualname = type_.__qualname__
+    #     repr_org = f"<{module}.{qualname} object at {hex(id(self))}>"
+    #     return repr_org + " :: " + self.__str__()
+
+    @property
+    def f1(self) -> float:
+        return self.omega[0]/2/np.pi
+
+    @property
+    def nfreq(self) -> int:
+        """Number of frequencies"""
+        return len(self.omega)
+
+    @property
+    def freq(self) -> np.ndarray:
+        return self.omega/2/np.pi
+
+    @property
+    def Zd(self) -> np.ndarray:
+        """Drive train impedance"""
+        return 1j * self.omega * self.Jd + self.Bd \
+            + self.Kd / (1j*self.omega)
+
+    @property
+    def Zw(self) -> np.ndarray:
+        """Winding impedance"""
+        return self.Rw + 1j * self.omega * self.Lw
+
+    @property
+    def Zpto(self) -> np.ndarray:
+        """PTO impedance"""
+        return np.array([
+            [self.Zd*self.N**2, -self.Kt*self.N*np.ones_like(self.omega)],
+            [-self.Kt*self.N*np.ones_like(self.omega), self.Zw]])
+
+    @property
+    def Zout(self) -> np.ndarray:
+        """PTO output impedance"""
+        return __Zout__(self.Zpto, self.Zi)
+
+    @property
+    def Zl_opt(self) -> np.ndarray:
+        """Optimal load impedance"""
+        return np.conj(self.Zout)
+
+    @property
+    def hydrodynamic_resonance(self) -> float:
+        return self.freq[np.argmin(np.abs(np.imag(self.Zi)))]
+
+    @property
+    def Zl_opt_mech(self) -> np.ndarray:
+        """Load impedance for optimal mechanical power"""
+        Zlm = np.conj(self.Zi)
+        return self.Kt**2*self.N**2/(Zlm - self.N**2*self.Zd) - self.Zw
+
+    @property
+    def Z_Thevenin(self) -> np.ndarray:
+        """Thévenin impedance"""
+        return self.Zout
+
+    def F_Thevenin(self, Fexc) -> np.ndarray:
+        """Thévenin source"""
+        return __F_Thevenin__(self.Zpto, self.Zi, Fexc)
+
+    def Zin(self, Zl=None) -> np.ndarray:
+        """PTO input impedance"""
+        if Zl is None:
+            Zl = self.Zl_opt
+        return __Zin__(self.Zpto, Zl)
+
+    def Fexc(self, waves) -> np.ndarray:
+        """Excitation spectrum"""
+        return __Fexc__(self.Hexc, waves)
+
+    def power(self, Fexc, Zl=None):
+        """Complex power at load"""
+        if Zl is None:
+            Zl = self.Zl_opt
+        return __power__(self.Zpto, self.Zi, Fexc, Zl)
+
+    def active_power(self, Fexc, Zl=None):
+        """Active power at load"""
+        return __active_power__(self.power(Fexc, Zl))
+
+    def reactive_power(self, Fexc, Zl=None):
+        """Reactive power at load"""
+        return __reactive_power__(self.power(Fexc, Zl))
+
+    def apparent_power(self, Fexc, Zl=None):
+        """Apparent power at load"""
+        return __apparent_power__(self.power(Fexc, Zl))
+
+    def max_active_power(self, Fexc):
+        """Maximum active power"""
+        return __max_active_power__(self.Z_Thevenin, self.F_Thevenin(Fexc))
+
+    def copy(self):
+        return copy.deepcopy(self)
+
+
+def __max_active_power__(Z_Thevenin, F_Thevenin):
+    return np.abs(F_Thevenin)**2 / (8 * np.real(Z_Thevenin))
+
+
+def __F_Thevenin__(Zpto, Zi, Fexc):
+    return Fexc*Zpto[1, 0] / (Zi + Zpto[0, 0])
+
+
+def __power__(Zpto, Zi, Fexc, Zl=None) -> np.ndarray:
+    return np.abs(Fexc*Zpto[1, 0]
+                  / ((Zpto[1, 1] + Zl)*(Zi + Zpto[0, 0]) -
+                      Zpto[1, 0]*Zpto[0, 1]))**2 * 1/2*Zl
+
+
+def __active_power__(power):
+    return np.real(power)
+
+
+def __reactive_power__(power):
+    return np.im(power)
+
+
+def __apparent_power__(power):
+    return np.abs(power)
+
+
+def __Fexc__(Hexc, waves) -> np.ndarray:
+    return Hexc * waves
+
+
+def __Zin__(Zpto, Zl) -> np.ndarray:
+    return Zpto[0, 0] - Zpto[0, 1] * Zpto[1, 0] / (Zpto[1, 1] + Zl)
+
+
+def __Zout__(Zpto, Zi) -> np.ndarray:
+    return Zpto[1, 1] - Zpto[1, 0] * Zpto[0, 1] / (Zi + Zpto[0, 0])
