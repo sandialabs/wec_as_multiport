eb451904136fe8d596f21d7076d753094991806c
Ryan Coe <rcoe@sandia.gov>
2024-06-10 16:49:44 -0600

    fixed a bug in definition of Fpto; add transducer gain
    Fpto[1,0] needed to be positive (was negative)

--- src/wec_as_multiport.py
+++ src/wec_as_multiport.py
@@ -55,10 +55,31 @@ class WEC:
 
     @property
     def Zpto(self) -> np.ndarray:
-        """PTO impedance"""
+        """PTO matrix in impedance form"""
         return np.array([
             [self.Zd*self.N**2, -self.Kt*self.N*np.ones_like(self.omega)],
-            [-self.Kt*self.N*np.ones_like(self.omega), self.Zw]])
+            [self.Kt*self.N*np.ones_like(self.omega), self.Zw]])
+
+    @property
+    def Ypto(self) -> np.ndarray:
+        """PTO matrix in admittance form"""
+        return 1/self.__detZpto__ * np.array([
+            [self.Zpto[1, 1], -1*self.Zpto[0, 1]],
+            [-1*self.Zpto[1, 0], self.Zpto[0, 0]]])
+
+    @property
+    def ABCDpto(self) -> np.ndarray:
+        """PTO matrix in ABCD form"""
+        return 1/self.Zpto[1, 0] * np.array([
+            [self.Zpto[0, 0], self.__detZpto__],
+            [np.ones_like(self.omega), self.Zpto[1, 1]]])
+
+    @property
+    def Hpto(self) -> np.ndarray:
+        """PTO matrix in hybrid form"""
+        return 1/self.Zpto[1, 1] * np.array([
+            [self.__detZpto__, self.Zpto[0, 1]],
+            [-1*self.Zpto[1, 0], np.ones_like(self.omega)]])
 
     @property
     def Zout(self) -> np.ndarray:
@@ -71,8 +92,14 @@ class WEC:
         return np.conj(self.Zout)
 
     @property
+    def hydrodynamic_resonance_index(self) -> int:
+        """Index of hydrodynamic resonance frequency"""
+        return np.argmin(np.abs(np.imag(self.Zi)))
+
+    @property
     def hydrodynamic_resonance(self) -> float:
-        return self.freq[np.argmin(np.abs(np.imag(self.Zi)))]
+        """Hydrodynamic resonant frequency"""
+        return self.freq[self.hydrodynamic_resonance_index]
 
     @property
     def Zl_opt_mech(self) -> np.ndarray:
@@ -85,6 +112,41 @@ class WEC:
     def Z_Thevenin(self) -> np.ndarray:
         """Thévenin impedance"""
         return self.Zout
+
+    @property
+    def __detZpto__(self) -> np.array:
+        """Determinant of Zpto"""
+        return np.linalg.det(np.transpose(self.Zpto, [2, 0, 1]))
+
+    # TODO
+    # def Fpto(self, Fexc, Zl=None) -> np.ndarray:
+    #     """PTO force"""
+    #     if Zl is None:
+    #         Fpto = self.velocity(Fexc=Fexc, Fpto=None) * self.Zin(Zl=Zl)
+    #     else:
+    #         raise NotImplementedError() #TODO
+    #     return Fpto
+
+    # def velocity(self, Fexc, Fpto=None) -> np.ndarray:
+    #     """Rectilinear velocity"""
+    #     if Fpto is None:
+    #         v = Fexc / (self.Zi + self.Zin(Zl=self.Zl_opt))
+    #     else:
+    #         v = (Fexc - Fpto) / self.Zi
+    #     return v
+
+    def transducer_power_gain(self, Zl=None) -> np.ndarray:
+        """Wave-to-wire efficiency (input power / power delivered to load)
+        https://en.wikipedia.org/wiki/Power_gain#Transducer_power_gain"""
+        if Zl is None:
+            Zl = self.Zl_opt
+        return 4*np.abs(self.Zpto[1, 0])**2 * np.real(Zl)*np.real(self.Zi) \
+            / np.abs((self.Zpto[0, 0] + self.Zi)*(self.Zpto[1, 1] + Zl) \
+                - self.Zpto[0, 1]*self.Zpto[1, 0])**2
+
+    def Zlm(self, Zl) -> np.ndarray:
+        """Mechanical load impedance"""
+        return self.Zin(Zl=Zl)
 
     def F_Thevenin(self, Fexc) -> np.ndarray:
         """Thévenin source"""
@@ -103,11 +165,11 @@ class WEC:
     def power_mech(self, Fexc, Zl=None):
         if Zl is None:
             Zl = self.Zl_opt
-        raise NotImplementedError() #TODO
+        raise NotImplementedError()  # TODO
 
     def power(self, Fexc, Zl=None):
         """Complex power at load"""
-        #TODO - define in terms of Vout and Iout
+        # TODO - define in terms of Vout and Iout
         if Zl is None:
             Zl = self.Zl_opt
         return __power__(self.Zpto, self.Zi, Fexc, Zl)
@@ -127,20 +189,25 @@ class WEC:
     def max_active_power(self, Fexc):
         """Maximum active power"""
         return __max_active_power__(self.Z_Thevenin, self.F_Thevenin(Fexc))
+    
+    def max_active_power_mech(self, Fexc):
+        """Maximum active mechanical power"""
+        return __max_active_power__(self.Zi, Fexc)
 
     def copy(self):
         return copy.deepcopy(self)
 
 
-def __max_active_power__(Z_Thevenin, F_Thevenin):
-    return np.abs(F_Thevenin)**2 / (8 * np.real(Z_Thevenin))
+def __max_active_power__(Z, F):
+    return np.abs(F)**2 / (8 * np.real(Z))
 
 
 def __F_Thevenin__(Zpto, Zi, Fexc):
     return Fexc*Zpto[1, 0] / (Zi + Zpto[0, 0])
 
+
 def __power__(Zpto, Zi, Fexc, Zl=None) -> np.ndarray:
-    #TODO - define in terms of flow and effort
+    # TODO - define in terms of flow and effort
     return np.abs(Fexc*Zpto[1, 0]
                   / ((Zpto[1, 1] + Zl)*(Zi + Zpto[0, 0]) -
                       Zpto[1, 0]*Zpto[0, 1]))**2 * 1/2*Zl
